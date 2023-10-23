3892a060fd5edfc50d2b334eb8d096f459b1dba1
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-10-23 12:15:32 +0200

    minor(navbar): navbar with conditional rendering

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { useAuthValue } from '../../context'
 
 const Navbar = () => {
+	const { user, isLoggedIn, logoutUser } = useAuthValue()
 	return (
 		<nav className='mb-8'>
 			<h2>
@@ -11,15 +13,30 @@ const Navbar = () => {
 			</h2>
 			<div>
 				<ul>
-					<li>
-						<Link to={'/signup'}>Sign Up</Link>
-					</li>
-					<li>
-						<Link to={'/login'}>Login</Link>
-					</li>
-					<li>
-						<Link to={'/logout'}>Logout</Link>
-					</li>
+					{!isLoggedIn ? (
+						<>
+							<li>
+								<Link to={'/signup'}>Sign Up</Link>
+							</li>
+							<li>
+								<Link to={'/login'}>Login</Link>
+							</li>
+						</>
+					) : (
+						<>
+							<p>Welcome, {user?.username}</p>
+							<li>
+								<a
+									className='text-red-400 bg-gray-800 px-2 py-2 rounded border-solid border-2 border-red-400 hover:cursor-pointer hover:shadow-sm hover:shadow-red-300'
+									onClick={() => {
+										logoutUser()
+									}}
+								>
+									Logout
+								</a>
+							</li>
+						</>
+					)}
 				</ul>
 			</div>
 		</nav>
