57a59496bb567e419798a64c0137dc959f158f5f
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-10-24 11:42:01 +0200

    minor(logout route): removed logout

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
 					<Route path='/' element={'Home'} />
 					<Route path='/signup' element={<Signup />} />
 					<Route path='/login' element={<Login />} />
-					<Route path='/logout' element={'logout'} />
 				</Routes>
 			</div>
 		</>
