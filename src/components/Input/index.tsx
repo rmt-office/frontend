b60fc61b4e9bb91f4fb7215f2722919c0fac805f
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-10-25 12:47:58 +0200

    feature(Input): Input as a component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,33 @@
+import { PropsWithRef, ReactNode, forwardRef } from 'react'
+
+interface InputProps extends PropsWithRef<JSX.IntrinsicElements['input']> {
+	type: React.DetailedHTMLProps<
+		React.InputHTMLAttributes<HTMLInputElement>,
+		HTMLInputElement
+	>['type']
+	id?: string
+	label: string
+	placeholder?: string
+	className?: string
+	children?: ReactNode
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ children, type, id, label, placeholder, className, ...props }, ref) => {
+		return (
+			<div className='grid gap-1'>
+				<label htmlFor={id}>{label}:</label>
+				<input
+					type={type}
+					id={id}
+					className={`rounded text-black placeholder:text-slate-400 px-1 py-1 ${className}`}
+					placeholder={placeholder}
+					ref={ref}
+					{...props}
+				/>
+				{children}
+			</div>
+		)
+	}
+)
+export default Input
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { authService } from '../../utils/services'
 import { AxiosError } from 'axios'
 import Button from '../../components/Button'
+import Input from '../../components/Input'
 
 const Signup = () => {
 	const {
@@ -33,7 +34,8 @@ const Signup = () => {
 				confirmPassword: values.confirmPassword,
 				username: values.username,
 			}
-
+			console.log(newUser)
+			return
 			const { data } = await authService.signup(newUser)
 			//TODO: Redirect the user after signup?
 			console.log(data)
@@ -50,74 +52,67 @@ const Signup = () => {
 		<>
 			<h1 className='mb-5 text-4xl'>Sign Up</h1>
 			<form className='flex flex-col gap-3 ' onSubmit={handleSubmit(onSubmit)}>
-				<div className='grid gap-1'>
-					<label htmlFor='username'>Username:</label>
-					<input
-						type='username'
-						id='username'
-						className='rounded text-black placeholder:text-slate-400 px-1 py-1'
-						placeholder='JohnDoe'
-						{...register('username')}
-					/>
+				<Input
+					type='text'
+					label='Username'
+					id='username'
+					placeholder='JohnDoe'
+					{...register('username')}
+				>
 					<legend className='mx-0.5 text-sm'>
 						<p>Should be unique</p>
 						<p>If you don't pick an username the first part of the email will be used</p>
 					</legend>
 					{errors.username && <p>{`${errors.username.message}`}</p>}
-				</div>
+				</Input>
 
-				<div className='grid gap-1'>
-					<label htmlFor='email'>Email*:</label>
-					<input
-						type='email'
-						id='email'
-						className='rounded text-black placeholder:text-slate-400 px-1 py-1'
-						placeholder='johndoe@gmail.com'
-						{...register('email', {
-							required: 'Required field',
-							pattern: {
-								value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-								message: 'Invalid email address',
-							},
-						})}
-					/>
+				<Input
+					type='email'
+					label='Email*'
+					id='email'
+					placeholder='johndoe@gmail.com'
+					{...register('email', {
+						required: 'Required field',
+						pattern: {
+							value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+							message: 'Invalid email address',
+						},
+					})}
+				>
 					<legend className='mx-0.5 text-sm'>
 						<p>Should be unique</p>
 					</legend>
 					{errors.email && <p className='text-red-400 mx-0.5'>{`${errors.email.message}`}</p>}
-				</div>
+				</Input>
 
-				<div className='grid gap-1'>
-					<label htmlFor='password'>Password*:</label>
-					<input
-						type='password'
-						id='password'
-						className='rounded text-black placeholder:text-slate-400 px-1 py-1'
-						placeholder='*********'
-						{...register('password', {
-							required: 'Required field',
-							pattern: {
-								value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
-								message: `Invalid password pattern`,
-							},
-						})}
-					/>
+				<Input
+					type='password'
+					id='password'
+					label='Password*'
+					{...register('password', {
+						required: 'Required field',
+						pattern: {
+							value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+							message: `Invalid password pattern`,
+						},
+					})}
+					placeholder='********'
+				>
+					{' '}
 					{errors.password && <p className='text-red-400 mx-0.5'>{`${errors.password.message}`}</p>}
-				</div>
+				</Input>
 
-				<div className='grid gap-1'>
-					<label htmlFor='confirmPassword'>Confirm Password*:</label>
-					<input
-						type='password'
-						id='confirmPassword'
-						className='rounded text-black placeholder:text-slate-400 px-1 py-1'
-						placeholder='*********'
-						{...register('confirmPassword', {
-							required: 'Required field',
-							validate: (value, formValues) =>
-								value !== formValues.password ? `Password don't match` : true,
-						})}
-					/>
+				<Input
+					type='password'
+					id='confirmPassword'
+					placeholder='*********'
+					label='Confirm Password*'
+					{...register('confirmPassword', {
+						required: 'Required field',
+						validate: (value, formValues) =>
+							value !== formValues.password ? `Password don't match` : true,
+					})}
+				>
 					{errors.confirmPassword && (
 						<p className='text-red-400 mx-0.5'>{`${errors.confirmPassword.message}`}</p>
 					)}
@@ -126,13 +121,13 @@ const Signup = () => {
 						<p>An uppercase letter, an lowercase letter, a number</p>
 						<p>And one of the following: #?!@$ %^&*-</p>
 					</legend>
-				</div>
+				</Input>
 
 				{errors.root?.serverError && (
 					<p className='text-red-400 mx-0.5 text-xl'>{errors.root.serverError.message}</p>
 				)}
 				<div className='self-end flex gap-1.5 mt-1'>
-					<Button>Register</Button>
+					<Button type='submit'>Register</Button>
 					<Button type='reset' onClick={() => reset()}>
 						Cancel
 					</Button>
