7543beef83c6fc89f9f536aae0bf82c646dfacbc
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-10-25 14:55:04 +0200

    minor(Home component): added home component to the routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<div className='w-5/6 mx-auto'>
+			<div className='w-100 mx-auto'>
 				<Routes>
-					<Route path='/' element={'Home'} />
+					<Route path='/' element={<Home />} />
 					<Route path='/signup' element={<Signup />} />
 					<Route path='/login' element={<Login />} />
 				</Routes>
