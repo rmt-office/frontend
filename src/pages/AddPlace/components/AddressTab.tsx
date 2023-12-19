c4ad44feffccfb390b3c9a5f9cdb4a52a9a648f9
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-12-19 13:37:18 +0100

    minor(address tab): added error messaging

--- src/pages/AddPlace/components/AddressTab.tsx
+++ src/pages/AddPlace/components/AddressTab.tsx
@@ -5,7 +5,6 @@ import { ErrorField, RegisterType } from '../types';
 const CATEGORY = ['Café', 'Airport', 'Hotel Lobby', 'Library', 'Coworking'];
 
 const AddressTab = ({ register, errors }: { register: RegisterType; errors: ErrorField }) => {
-	console.log(errors);
 	return (
 		<>
 			<Input
@@ -19,7 +18,7 @@ const AddressTab = ({ register, errors }: { register: RegisterType; errors: Erro
 			</Input>
 			<label htmlFor='category'>Category*</label>
 			<select
-				required			
+				required
 				className='rounded text-black px-1 py-1 invalid:text-slate-400'
 				id='category'
 				{...register('category', {
@@ -41,7 +40,7 @@ const AddressTab = ({ register, errors }: { register: RegisterType; errors: Erro
 				type='text'
 				id='country'
 				placeholder='France'
-				{...register('address.country')}
+				{...register('address.country', { required: 'Country is required' })}
 			>
 				{errors?.address?.country && <FormError message={errors.address?.country.message} />}
 			</Input>
@@ -50,11 +49,17 @@ const AddressTab = ({ register, errors }: { register: RegisterType; errors: Erro
 				type='text'
 				id='postalCode'
 				placeholder='75000'
-				{...register('address.zipCode')}
+				{...register('address.zipCode', {required: 'Inform us the zip code'})}
 			>
 				{errors?.address?.zipCode && <FormError message={errors.address?.zipCode.message} />}
 			</Input>
-			<Input label='City*' type='text' id='city' placeholder='Paris' {...register('address.city')}>
+			<Input
+				label='City*'
+				type='text'
+				id='city'
+				placeholder='Paris'
+				{...register('address.city', { required: 'A city is required' })}
+			>
 				{errors?.address?.city && <FormError message={errors.address?.city.message} />}
 			</Input>
 			<Input
@@ -62,7 +67,7 @@ const AddressTab = ({ register, errors }: { register: RegisterType; errors: Erro
 				type='text'
 				id='street'
 				placeholder='Av. des Champs-Élysées'
-				{...register('address.street')}
+				{...register('address.street', {required: 'Which street the remote space is located?'})}
 			>
 				{errors?.address?.street && <FormError message={errors.address?.street.message} />}
 			</Input>
