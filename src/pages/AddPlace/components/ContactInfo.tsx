e28ae32dc979389110282d984681944eaafbd1e4
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-12-19 13:38:12 +0100

    feature(dynamic form - WIP): implementing the dunamic form inputs

--- src/pages/AddPlace/components/ContactInfo.tsx
+++ src/pages/AddPlace/components/ContactInfo.tsx
@@ -1,19 +1,71 @@
+import { useState } from 'react';
+import FormError from '../../../components/FormError';
 import Input from '../../../components/Input';
-import { RegisterType } from '../types';
+import { ContactInfoTypes, ErrorField, RegisterType, UnregisterType } from '../types';
 
-const ContactInfo = ({ register }: { register: RegisterType }) => {
+const ContactInfo = ({
+	register,
+	unregister,
+	errors,
+}: {
+	register: RegisterType;
+	unregister: UnregisterType;
+	errors: ErrorField;
+}) => {
+	const [contactInfoFields, setContactInfoFields] = useState<string[]>([]);
 	return (
-		<div>
-			<Input
+		<>
+			{contactInfoFields.map((ci) => {
+				const key = ci as keyof ContactInfoTypes;
+
+				return (
+					<Input
+						label={`${ci}`}
+						type='text'
+						id='website'
+						placeholder='www.facebook.com'
+						{...register(`contactInfo.${key}`)}
+					>
+						<span
+							className='hover:cursor-pointer'
+							onClick={() => {
+								setContactInfoFields((previousFields) => {
+									const newFields = previousFields.filter((field) => field !== key);
+									return newFields;
+								});
+								unregister(`contactInfo.${key}`);
+							}}
+						>
+							remover
+						</span>
+						{errors.contactInfo && <FormError message={errors.contactInfo?.website?.message} />}
+					</Input>
+				);
+			})}
+			<select
+				onChange={(e) => {
+					setContactInfoFields([...contactInfoFields, e.target.value]);
+				}}
+			>
+				<option value='website'>Website</option>
+				<option value='telephone'>Phone</option>
+				<option value='instagram'>Instagram</option>
+				<option value='facebook'>Facebook</option>
+			</select>
+			{/* <Input
 				label='Website*'
 				type='text'
 				id='website'
 				placeholder='www.facebook.com'
 				{...register('contactInfo.website')}
-			></Input>
-			<Input label='Email*' type='text' id='type' placeholder='Type'></Input>
-			<p>We need at least one way to contact the establishment, ex: website, phone, instagram</p>
-		</div>
+			>
+				{errors.contactInfo && <FormError message={errors.contactInfo?.website?.message} />}
+			</Input> */}
+			{/* <Input label='Phone*' type='number' id='phone' placeholder='9 99 99 99 99' inputMode='tel' {...register('contactInfo.telephone')}></Input>
+			<p>We need at least one way to contact the establishment, ex: website, phone, instagram</p> */}
+
+			{errors.contactInfo && <FormError message={errors.contactInfo?.message} />}
+		</>
 	);
 };
 export default ContactInfo;
