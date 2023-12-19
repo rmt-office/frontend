2fac565f0a7ccbb0c30f8df1e1cf01e9df7281a3
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-12-19 13:35:12 +0100

    minor(New Place types): fixing the types for the new place

--- src/utils/services/placeService.ts
+++ src/utils/services/placeService.ts
@@ -1,30 +1,34 @@
-import { ApiConnect } from '../../lib/axios'
+import { ApiConnect } from '../../lib/axios';
 
 type NewPlace = {
-  name: string,
-			category: string,
-			contactInfo: {
-				website: string,
-			},
-			price?: number,
-			meetingRooms?: number,
-			address: {
-				country: string,
-				city: string,
-				street: string,
-				zipCode: string,
-			},
-}
+	name: string;
+	category: string;
+	contactInfo: {
+		website?: string;
+		telephone?: string;
+		facebook?: string;
+		linkedIn?: string;
+		instagram?: string;
+	};
+	price?: number;
+	meetingRooms?: number;
+	address: {
+		country: string;
+		city: string;
+		street: string;
+		zipCode: string;
+	};
+};
 
 class PlaceService extends ApiConnect {
 	constructor() {
-		super('places')
+		super('places');
 	}
 
 	async createPlace(place: NewPlace) {
-		return this.post<NewPlace, {data: NewPlace}>('/', place)
+		return this.post<NewPlace, { data: NewPlace }>('/', place);
 	}
 }
 
-const placeService = new PlaceService()
-export { placeService }
+const placeService = new PlaceService();
+export { placeService };
